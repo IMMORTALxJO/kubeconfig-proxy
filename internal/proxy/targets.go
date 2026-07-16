686bf68a519a4a117d2562315c95edd6a0b590d6
IMMORTALxJO <iblaaaz@gmail.com>
2026-07-16 17:20:57 +0300

    kubeconfig oicd

--- internal/proxy/targets.go
+++ internal/proxy/targets.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 	"strings"
 
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // register legacy kubeconfig OIDC auth-provider
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
